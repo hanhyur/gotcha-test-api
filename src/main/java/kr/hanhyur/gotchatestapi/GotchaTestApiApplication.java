2fdbfd18616502ff1b11d7314b59e296bac13b02
hanhyur <zeidgame@gmail.com>
2023-01-18 10:54:30 +0900

    feat : 가챠 시뮬 api 프로젝트 생성
    <description>
    [+]
    >> 기본 프로젝트 생성
    
    <issue-number>

---
+++ src/main/java/kr/hanhyur/gotchatestapi/GotchaTestApiApplication.java
@@ -0,0 +1,13 @@
+package kr.hanhyur.gotchatestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchaTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotchaTestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hanhyur/gotchatestapi/GotchaTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hanhyur.gotchatestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotchaTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
